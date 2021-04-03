f53f95486693bcbf8bc57d20cbfe77271c6fde36
DevaNurdiyawanto-HackerOne <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 00:59:48 +0700

    Create TermuxInstall.sh

---
+++ TermuxInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pkg install git php curl wget nano python python2 nmap ruby vim nodejs electrum numpy scipy build-essential cmake libjpeg-turbo libpng python make yarn perl -y
+pip install matplotlib numpy
+gem install nokogiri -- --use-system-libraries
+gem install thin -- --use-system-libraries
