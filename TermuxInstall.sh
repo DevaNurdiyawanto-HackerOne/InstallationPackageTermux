595d2d79788f7d28366cd8edb0be725b3e4fb049
DevaNurdiyawanto-HackerOne <81873740+DevaNurdiyawanto-HackerOne@users.noreply.github.com>
2021-04-04 01:14:55 +0700

    Update TermuxInstall.sh

--- TermuxInstall.sh
+++ TermuxInstall.sh
@@ -1,1 +1,1 @@
-pkg install git php curl wget nano python python2 nmap ruby vim nodejs electrum numpy scipy build-essential cmake libjpeg-turbo libpng python make yarn perl -y
+pkg install git php curl wget nano python python2 nmap ruby vim nodejs electrum build-essential cmake libjpeg-turbo libpng python make yarn perl -y
